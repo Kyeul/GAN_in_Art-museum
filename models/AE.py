03d94055811502cf12eff1eb9e8fe457dfb39b42
Kyeul <gyul611@gmail.com>
2020-07-01 14:24:52 +0000

    add code

--- models/AE.py
+++ models/AE.py
@@ -15,7 +15,7 @@ import os
 import pickle
 
 class Autoencoder():
-  def __init__(self, input_dim, encoder_conv_filters, encoder_conv_kernel_size, encoder_conv_striders, decoder_conv_t_filters, decoder_conv_t_kernel_size, decoder_conv_t_striders, z_dim, use_batch_norm = False, use_dropout = False):
+  def __init__(self, input_dim, encoder_conv_filters, encoder_conv_kernel_size, encoder_conv_strides, decoder_conv_t_filters, decoder_conv_t_kernel_size, decoder_conv_t_strides, z_dim, use_batch_norm = False, use_dropout = False):
 
     self.name = 'autoencoder'
 
@@ -26,13 +26,13 @@ class Autoencoder():
     self.decoder_conv_t_filters = decoder_conv_t_filters
     self.decoder_conv_t_kernel_size = decoder_conv_t_kernel_size
     self.decoder_conv_t_strides = decoder_conv_t_strides
-    self.z_dim = z.dim
+    self.z_dim = z_dim
 
     self.use_batch_norm = use_batch_norm
     self.use_dropout = use_dropout
 
     self.n_layers_encoder = len(encoder_conv_filters)
-    self.n_lyaers_decoder = len(decoder_conv_filters)
+    self.n_layers_decoder = len(decoder_conv_t_filters)
 
     self._build()
 
@@ -75,7 +75,7 @@ class Autoencoder():
 
       decoder_input = Input(shape=(self.z_dim,), name='decoder_input')
 
-      x = Dense(np.prop(shape_before_flattening))(decoder_input)
+      x = Dense(np.prod(shape_before_flattening))(decoder_input)
       x = Reshape(shape_before_flattening)(x)
 
       for i in range(self.n_layers_decoder):
